7da1581324af9cdfc78c9eef2beea7aa4410af02
DanielSantaz <73202278+DanielSantaz@users.noreply.github.com>
2022-12-11 17:38:52 -0500

    Add files via upload

---
+++ Pet/src/Pet.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author d4ni3
+ */
+public class Pet {
+    String name = "";
+    int hunger = 0;
+    int boredom = 0;
+    
+    
+}
